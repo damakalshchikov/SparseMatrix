58f991fb10e99298cd759d3d74d301577b6c5cdf
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-23 02:02:42 +0300

    Добавлен вывод полной разрежённой матрицы

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import numpy as np
 from funcsions.read_file import read_file_to_sparse_matrix
 
 file_path = "./Cases/test.txt"
 A = read_file_to_sparse_matrix(file_path)
-print(A)
+
+matrix = np.array(A.todense())
+print(matrix)
