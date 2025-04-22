b2381113094a76e83f7b5c176433d35109aa3bb6
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-23 02:52:53 +0300

    Определено тело функции циклического сдвига вправо

--- funcsions/cyclical_shift.py
+++ funcsions/cyclical_shift.py
@@ -1,11 +1,35 @@
+from scipy.sparse import coo_matrix
+
+
 def shift(matrix):
     """
     Функция для циклического сдвига матрицы на 1 позицию вправо.
 
     Parameters:
-        matrix (numpy.ndarray): Исходная матрица.
+        matrix (scipy.sparse._coo.coo_matrix): Исходная матрица.
 
     Returns:
-        numpy.ndarray: Циклически сдвинутая матрица.
+        scipy.sparse._coo.coo_matrix: Циклически сдвинутая матрица.
     """
-    ...
+
+    # Размеры матрицы
+    n_rows, n_cols = matrix.shape
+
+    # Вычисляем линейные индексы элементов
+    # Линейные индексы элементов - это одномерные индексы,
+    # которые соответствуют двумерным индексам
+    linear_indices = matrix.row * n_cols + matrix.col
+
+    # Сдвиг линейных индексов на 1 позицию вправо
+    shifted_linear_indices = (linear_indices + 1) % (n_rows * n_cols)
+
+    # Преобразуем линейные индексы обратно в координаты строк и столбцов
+    new_rows = shifted_linear_indices // n_cols
+    new_cols = shifted_linear_indices % n_cols
+
+    # Создаем новую разреженную матрицу с обновленными координатами
+    shifted_matrix = coo_matrix(
+        (matrix.data, (new_rows, new_cols)), shape=matrix.shape
+    )
+
+    return shifted_matrix
