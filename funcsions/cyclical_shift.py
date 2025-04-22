cb217be7f1811de08a83da84064f06af5e19758b
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-23 02:07:58 +0300

    Определение заголовка функции циклического сдвига

---
+++ funcsions/cyclical_shift.py
@@ -0,0 +1,11 @@
+def shift(matrix):
+    """
+    Функция для циклического сдвига матрицы на 1 позицию вправо.
+
+    Parameters:
+        matrix (numpy.ndarray): Исходная матрица.
+
+    Returns:
+        numpy.ndarray: Циклически сдвинутая матрица.
+    """
+    ...
