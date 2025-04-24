47d8b3d4a9907b002d3b8ebea0d0133071e8ecd3
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-24 19:20:16 +0300

    Добавлена точка входа в проект

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import funcsions
 import matplotlib.pyplot as plt
 
 
-file_path = "./Cases/test.txt"
-A = funcsions.read_file_to_sparse_matrix(file_path)
+def main(path):
+    """
+    Точка входа
 
-print("Исходная матрица:")
-print(A.toarray(), "\n")
+    Args:
+        path: Путь к файлу, из которого будет считываться матрица
 
-B = funcsions.shift(A)
-print("Циклически сдвинутая матрица:")
-print(B.toarray())
+    Returns: None
+
+    """
+
+    a = funcsions.read_file_to_sparse_matrix(path)
+
+    print("Исходная матрица:")
+    print(a.toarray(), "\n")
+
+    b = funcsions.shift(a)
+    print("Циклически сдвинутая матрица:")
+    print(b.toarray())
+
+    funcsions.visualize_matrices(a.toarray(), b.toarray())
+
+    plt.savefig("./Images/Matrix.png", dpi=300, bbox_inches="tight")
+
 
 fig = funcsions.visualize_matrices(A.toarray(), B.toarray())
 
