9d52357c4a970e02fdefc1da94f9c6b5ab692c79
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-04-24 17:43:42 +0300

    Вызов функции визуализации матриц

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import funcsions
+import matplotlib.pyplot as plt
+
 
 file_path = "./Cases/test.txt"
 A = funcsions.read_file_to_sparse_matrix(file_path)
@@ -9,3 +11,9 @@ print(A.toarray(), "\n")
 B = funcsions.shift(A)
 print("Циклически сдвинутая матрица:")
 print(B.toarray())
+
+fig = funcsions.visualize_matrices(A.toarray(), B.toarray())
+
+plt.savefig("matrix_visualization.png", dpi=300, bbox_inches='tight')
+
+plt.show()
